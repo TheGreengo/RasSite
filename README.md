8dbc6ad7440a78bdf3cdd04cfe5289b0ec4c41b2
Braden <bradenoptimus@gmail.com>
2023-10-24 17:52:38 -0700

    Basic layout started

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # RasSite
 Just a simple webpage to run as a test on my raspberry pi. The goal is to serve a little checklist app (using flask) on my raspberry pi and see if I can go to that website on another computer on the same network.
 
-Of course, just a complet barebones text-only site would be no fun at all, so I figured I'd make a little checklist app.
+Of course, just a complete barebones text-only site would be no fun at all, so I figured I'd make a little checklist app.
--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
+todos = []
+
 @app.route("/")
 def index():
-    return render_template('main.html')
+    return render_template('main.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+    return render_template('main.html', todos=todos)
+
+@app.route('/remove', methods=['DELETE'])
+def remove():
+    return render_template('main.html', todos=todos)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
